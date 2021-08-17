d609e08ff702c7789d57f1e2a187542e7961bb5c
melburmer <melihgorgulu98@gmail.com>
2021-08-17 08:04:05 +0300

    new design for home page [ADD]

--- dfpproject/dfpproject/views.py
+++ dfpproject/dfpproject/views.py
@@ -1,4 +1,16 @@
 from django.views.generic import TemplateView
+from database.models import Records
+from django.shortcuts import render
+from django.db.models import Count
 
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+    def get(self, request):
+        target_document_keys = ["fo_use_case", "midas_version", "project",
+                                "region", "record_type", "activity",
+                                "soil_type", "time_of_day"]
+
+        count = Records.objects.values('soil_type').annotate(mcount=Count('soil_type')).order_by()
+
+        return render(request, self.template_name, {'target_document_keys':target_document_keys})
